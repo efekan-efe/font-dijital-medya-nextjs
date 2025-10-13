5c0a0ad375252b72f5e1d76f1bf6ab573624f504
Efekan EFE <efekanefos@gmail.com>
2025-10-13 09:05:07 +0300

    project start setup completed

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Projemize Hoş Geldin!</h1>
+      <p className="mt-2 text-lg text-gray-600">Frontend iskeletimiz hazır.</p>
+    </main>
+  );
+}
