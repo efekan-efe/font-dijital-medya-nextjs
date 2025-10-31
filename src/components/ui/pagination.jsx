121cb57e0106240bb020fb1aad8e092103794c43
Efekan EFE <efekanefos@gmail.com>
2025-10-31 12:16:41 +0300

    Update pagination.jsx

--- src/components/ui/pagination.jsx
+++ src/components/ui/pagination.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { ChevronLeftIcon, ChevronRightIcon, MoreHorizontalIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "./button";
+import { buttonVariants } from "./button.jsx";
 
 function Pagination({ className, ...props }) {
   return <nav role="navigation" aria-label="pagination" data-slot="pagination" className={cn("mx-auto flex w-full justify-center", className)} {...props} />;
