f291fe7c9c0056b3f753aaf5ec4e6c9e4583025f
Efekan EFE <efekanefos@gmail.com>
2026-01-07 17:23:35 +0300

    padding top to hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ import ProcessSection from "@/components/views/home/ProcessSection";
 export default function HomePage() {
   return (
     // homepageMain class'ı globals.css'te sectionları hedeflemek için hala gerekli
-    <main className="homepageMain flex flex-col">
+    <main className="homepageMain flex flex-col pt-5">
       {/* Sectionların CSS'te zaten height: 100vh ve snap-align: start özelliği var */}
       <HeroSection />
       <FeaturesSection />
