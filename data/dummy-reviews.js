451bf68aaed77774acc85db829ef4f7d4a5baa48
Efekan EFE <efekanefos@gmail.com>
2026-01-10 12:58:11 +0300

    google reviews added

--- data/dummy-reviews.js
+++ data/dummy-reviews.js
@@ -1,81 +1,411 @@
-import googleAvatar from "@/assets/homepage/images/google_avatar.png";
+import furkan_pak from "@/assets/google_reviews/furkan_pak.png";
+import ibrahim_elvan from "@/assets/google_reviews/ibrahim_elvan.png";
+import rahmi_kandemir from "@/assets/google_reviews/rahmi_kandemir.png";
+import aziz_can_kartal from "@/assets/google_reviews/aziz_can_kartal.png";
+import aykut_yuceant from "@/assets/google_reviews/aykut_yuceant.png";
+import aziz_ozyigit from "@/assets/google_reviews/aziz_ozyigit.png";
+import nihat_alperen_ozkan from "@/assets/google_reviews/nihat_alperen_ozkan.png";
+import serdar_solen from "@/assets/google_reviews/serdar_solen.png";
+import burak_bilgetekin from "@/assets/google_reviews/burak_bilgetekin.png";
+import mehmet_ozdemir from "@/assets/google_reviews/mehmet_ozdemir.png";
+import anil_can_iskey from "@/assets/google_reviews/anil_can_iskey.png";
+import yusuf_varli from "@/assets/google_reviews/yusuf_varli.png";
+import emel_karayavuz from "@/assets/google_reviews/emel_karayavuz.png";
+import fevzi_emre_ozyesilbas from "@/assets/google_reviews/fevzi_emre_ozyesilbas.png";
+import kubra_tasdelen from "@/assets/google_reviews/kubra_tasdelen.png";
+import abdulkadir_ozatac from "@/assets/google_reviews/abdulkadir_ozatac.png";
+import abdullah_celebi from "@/assets/google_reviews/abdullah_celebi.png";
+import ahmet_aydin from "@/assets/google_reviews/ahmet_aydin.png";
+import arda_gorgulu from "@/assets/google_reviews/arda_gorgulu.png";
+import asena_aydin from "@/assets/google_reviews/asena_aydin.png";
+import bilal_demir from "@/assets/google_reviews/bilal_demir.png";
+import bunyamin_ozdemir from "@/assets/google_reviews/bunyamin_ozdemir.png";
+import burak_demir from "@/assets/google_reviews/burak_demir.png";
+import busra_koksal from "@/assets/google_reviews/busra_koksal.png";
+import cevdet_demir from "@/assets/google_reviews/cevdet_demir.png";
+import ergun_akkurt from "@/assets/google_reviews/ergun_akkurt.png";
+import esra_tasdelen from "@/assets/google_reviews/esra_tasdelen.png";
+import fadime_ozdemir from "@/assets/google_reviews/fadime_ozdemir.png";
+import gulnur_pak from "@/assets/google_reviews/gulnur_pak.png";
+import gulseren_kusmus from "@/assets/google_reviews/gulseren_kusmus.png";
+import hatice_aybala_ates from "@/assets/google_reviews/hatice_aybala_ates.png";
+import suheda_reyhan_simsek from "@/assets/google_reviews/suheda_reyhan_simsek.png";
+import yaren_solen from "@/assets/google_reviews/yaren_solen.png";
+import yusuf_cam from "@/assets/google_reviews/yusuf_cam.png";
 
 export const dummyReviews = [
   {
     id: 1,
     author: {
-      name: "Uğur Bulut",
-      avatar: googleAvatar,
+      name: "Furkan Pak",
+      avatar: furkan_pak,
     },
     rating: 5,
     date: "28 Mart, 2023",
-    comment: '"Süperrrrrrr"',
+    comment: "",
     source: "Google",
   },
   {
     id: 2,
     author: {
-      name: "Ahmet Gündüz",
-      avatar: googleAvatar,
+      name: "İbrahim Elvan",
+      avatar: ibrahim_elvan,
     },
     rating: 5,
     date: "15 Nisan, 2023",
-    comment: '"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"',
+    comment: "",
     source: "Google",
   },
   {
     id: 3,
     author: {
-      name: "Ayşe Buhur",
-      avatar: googleAvatar,
+      name: "Rahmi Kandemir",
+      avatar: rahmi_kandemir,
     },
     rating: 5,
     date: "15 Nisan, 2023",
-    comment: '"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"',
+    comment: '"Profesyonel olarak hızlı ve güvenle kullandığımız e-ticaret siteleri sayesinde kolayca işlerimizi hallediyoruz. Font dijital ekibine teşekkürler."',
     source: "Google",
   },
   {
     id: 4,
     author: {
-      name: "Ayşe Yılmaz",
-      avatar: googleAvatar,
+      name: "Aziz Can Kartal",
+      avatar: aziz_can_kartal,
     },
     rating: 5,
     date: "15 Nisan, 2023",
-    comment: '"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"',
+    comment: "",
     source: "Google",
   },
   {
     id: 5,
     author: {
-      name: "Ayşe Buhur",
-      avatar: googleAvatar,
+      name: "Aykut Yüceant",
+      avatar: aykut_yuceant,
     },
     rating: 5,
     date: "15 Nisan, 2023",
-    comment: '"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"',
+    comment: "",
     source: "Google",
   },
   {
     id: 6,
     author: {
-      name: "Efekan EFE",
-      avatar: googleAvatar,
+      name: "Aziz Özyiğit",
+      avatar: aziz_ozyigit,
     },
     rating: 5,
     date: "15 Nisan, 2023",
-    comment: '"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"',
+    comment: `"Font Dijital Medya'dan reklam hizmeti aldık. İşletmemiz için güzel dönüşler aldık."`,
     source: "Google",
   },
   {
     id: 7,
     author: {
-      name: "Murat UZUN",
-      avatar: googleAvatar,
+      name: "Nihat Alperen Özkan",
+      avatar: nihat_alperen_ozkan,
     },
     rating: 5,
     date: "15 Nisan, 2023",
-    comment: '"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"',
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 8,
+    author: {
+      name: "Serdar Şölen",
+      avatar: serdar_solen,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 9,
+    author: {
+      name: "Burak Bilgetekin",
+      avatar: burak_bilgetekin,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `"İşletmemizin web sitesi çok kullanışlı kendilerine teşekkürler"`,
+    source: "Google",
+  },
+  {
+    id: 10,
+    author: {
+      name: "Mehmet Özdemir",
+      avatar: mehmet_ozdemir,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 11,
+    author: {
+      name: "Anıl Can İşkey",
+      avatar: anil_can_iskey,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 12,
+    author: {
+      name: "Yusuf Varlı",
+      avatar: yusuf_varli,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 13,
+    author: {
+      name: "Emel Karayavuz",
+      avatar: emel_karayavuz,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 14,
+    author: {
+      name: "Fevzi Emre Özyeşilbaş",
+      avatar: fevzi_emre_ozyesilbas,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `"Sosyal medya yönetimi konusunda çok tecrübeliler . Aldığım destekten ötürü Font Dijital’e çok teşekkür ederim"`,
+    source: "Google",
+  },
+  {
+    id: 15,
+    author: {
+      name: "Kübra Taşdelen",
+      avatar: kubra_tasdelen,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 16,
+    author: {
+      name: "Abdulkadir Özataç",
+      avatar: abdulkadir_ozatac,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 17,
+    author: {
+      name: "Abdullah Çelebi",
+      avatar: abdullah_celebi,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 18,
+    author: {
+      name: "Ahmet Aydın",
+      avatar: ahmet_aydin,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 19,
+    author: {
+      name: "Arda Görgülü",
+      avatar: arda_gorgulu,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 20,
+    author: {
+      name: "Asena Aydın",
+      avatar: asena_aydin,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 21,
+    author: {
+      name: "Bilal Demir",
+      avatar: bilal_demir,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 22,
+    author: {
+      name: "Bünyamin Özdemir",
+      avatar: bunyamin_ozdemir,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 23,
+    author: {
+      name: "Burak Demir",
+      avatar: burak_demir,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 24,
+    author: {
+      name: "Büşra Köksal",
+      avatar: busra_koksal,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 25,
+    author: {
+      name: "Cevdet Demir",
+      avatar: cevdet_demir,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 26,
+    author: {
+      name: "Ergün Akkurt",
+      avatar: ergun_akkurt,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `"İşinde çok başarılı bir dijital medya, oldukça fazla iş yaptırma fırsatımız oldu, gayet profesyonel işler çıkartıldı, Müşterisini iyi dinleyen, Güzel anlayan bir firma."`,
+    source: "Google",
+  },
+  {
+    id: 27,
+    author: {
+      name: "Esra Taşdelen",
+      avatar: esra_tasdelen,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 28,
+    author: {
+      name: "Fadime Özdemir",
+      avatar: fadime_ozdemir,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 29,
+    author: {
+      name: "Gülnur Pak",
+      avatar: gulnur_pak,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 30,
+    author: {
+      name: "Gülseren Küsmüş",
+      avatar: gulseren_kusmus,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `"Eğitim sayesinde dijital pazarlama stratejilerimi geliştirdim ve online varlığımı güçlendirdim. Eğitim çok faydalı ve yol göstericiydi."`,
+    source: "Google",
+  },
+  {
+    id: 31,
+    author: {
+      name: "Hatice Aybala Ateş",
+      avatar: hatice_aybala_ates,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 32,
+    author: {
+      name: "Şüheda Reyhan Şimşek",
+      avatar: suheda_reyhan_simsek,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
+    source: "Google",
+  },
+  {
+    id: 33,
+    author: {
+      name: "Yaren Şölen",
+      avatar: yaren_solen,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `"Font Dijital Medya'ya Diş Kliniğimizin reklam yönetimi işlerini teslim ettik ve aldığımız dönüşler bizleri memnun etti."`,
+    source: "Google",
+  },
+  {
+    id: 34,
+    author: {
+      name: "Yusuf Çam",
+      avatar: yusuf_cam,
+    },
+    rating: 5,
+    date: "15 Nisan, 2023",
+    comment: `""`,
     source: "Google",
   },
 ];
